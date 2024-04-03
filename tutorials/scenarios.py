1da2755cb080f8c4924f2d7e52556b1da0d375f6
Cliff Kerr <questionably@gmail.com>
2024-04-03 00:01:22 -0400

    working on scenarios tutorial

--- tutorials/scenarios.py
+++ tutorials/scenarios.py
@@ -3,6 +3,25 @@ import numpy as np
 import pylab as pl
 import starsim as ss
 
+data_baseline = np.array([
+ 1,  1,  1,  1,  2,  4, 10, 15, 22, 31, 45, 59, 75, 83, 90, 89,
+85, 83, 79, 74, 64, 53, 46, 44, 43, 45, 51, 55, 57, 57, 61, 53,
+51, 48, 40, 34, 29, 27, 22, 18, 16, 13, 11, 12, 12, 12, 13, 13,
+14, 12, 13, 13, 13, 15, 17, 18, 19, 22, 24, 29, 38, 40, 44, 50,
+53, 55, 58, 56, 53, 52, 48, 42, 37, 31, 28, 19, 16, 15, 13, 13,
+12, 14, 13, 11, 13, 11, 13, 14, 15, 15, 13, 14, 16, 21, 21, 28,
+36, 40, 42, 48, 49])
+
+data_vaccine = np.array([
+ 1,  1,  1,  1,  2,  4, 10, 15, 22, 31, 45, 48, 55, 58, 56, 53,
+49, 46, 38, 29, 24, 20, 17, 15, 16, 15, 20, 20, 21, 24, 26, 21,
+23, 23, 24, 22, 18, 16, 15, 14, 12,  8,  6,  6,  5,  6,  6,  4,
+ 4,  3,  3,  3,  2,  4,  5,  7,  9, 10, 12, 14, 17, 19, 18, 20,
+22, 23, 25, 24, 23, 23, 19, 19, 19, 15, 14, 11, 11, 10,  7,  6,
+ 6,  5,  3,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+ 0,  0,  0,  0,  0])
+
+
 class Vaccine(ss.Intervention):
     def __init__(self, ti=10, p=0.5, boost=2.0):
         super().__init__()
@@ -20,7 +39,7 @@ class Vaccine(ss.Intervention):
             sis.immunity[vacc_ids] += self.boost
 
 
-def make_run_sim(beta=0.05, waning=0.05, seed=1, vaccinate=False, p=0.5, boost=2.0):
+def make_run_sim(beta=0.05, waning=0.05, seed=1, vaccine=False, p=0.5, boost=2.0):
     pars = dict(
         n_agents = 100,
         start = 0,
@@ -36,7 +55,7 @@ def make_run_sim(beta=0.05, waning=0.05, seed=1, vaccinate=False, p=0.5, boost=2
     )
     
     # Define "baseline" and "intervention" sims without and with the vaccine
-    if vaccinate:
+    if vaccine:
         vx = Vaccine(p=p, boost=boost)
         sim = ss.Sim(pars, interventions=vx)
     else:
@@ -50,18 +69,23 @@ def make_run_sim(beta=0.05, waning=0.05, seed=1, vaccinate=False, p=0.5, boost=2
     return results
 
 
-def plot(results):
-    # pl.figure()
+def plot(results, label=''):
     pl.title('Number of people infected')
-    pl.plot(results.time, results.n_infected, 'o-', label='Baseline') # Plot baseline
+    pl.plot(results.time, results.n_infected, 'o-', label=label)
     pl.legend()
     sc.figlayout()
     pl.show()
     
+
 # Make, run, and plot the simulation
 sc.options(dpi=200)
-for i in range(10):
-    results = make_run_sim(beta=0.08, waning=0.03, seed=i)
-    plot(results)
-    pl.title(i)
-    pl.pause(0.5)
+pars = dict(beta=0.08, waning=0.03, seed=9)
+res1 = make_run_sim(**pars)
+plot(res1, 'Baseline')
+res2 = make_run_sim(**pars, vaccine=True, p=0.7, boost=10)
+plot(res2, 'Vaccine')
+
+# Save data
+print(res1.n_infected.astype(int))
+print(res2.n_infected.astype(int))
+
